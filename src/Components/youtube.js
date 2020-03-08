d94c2a39e626ce6013e4b209c8beab6734dadff5
Muamer Kukic <kukicako@gmail.com>
2020-03-07 19:18:56 -0800

    flip card

--- src/Components/youtube.js
+++ src/Components/youtube.js
@@ -13,7 +13,7 @@ const Youtube = props => {
         width="700" 
         height="400" 
         src="https://www.youtube.com/embed/HZu29rVkmzU" 
-        frameborder="0" 
+        frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
         </div>
