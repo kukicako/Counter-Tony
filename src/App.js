3f3746eb0d04c76e617e0b2d8c32a90ce4ffd3aa
Muamer Kukic <kukicako@gmail.com>
2020-03-07 17:38:08 -0800

    logo and links done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
-import HomeCarousel from './Components/carousel';
-import Nav from './Components/nav.js'
-import Footer from './Components/footer.js'
+import HomeCarousel from './components/carousel';
+import Nav from './components/nav.js'
+import Footer from './components/footer.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
       <HomeCarousel />
       <Footer />
     </div>
+    </Router>
   );
 }
 
