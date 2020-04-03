7a7a5f2804022a0da8967290ce53a57bbedd42a2
Gatrrr <davidgatrmacaulay@gmail.com>
2020-04-02 18:34:18 -0700

    Fixed Sidebar

--- src/Components/Guides/guidetext.js
+++ src/Components/Guides/guidetext.js
@@ -9,22 +9,28 @@ function GuideText () {
         it to better myself in the end as a player and IGL. Today, I wanted to give a revised version of my In Game Leader guide to potentially help new IGLs or  
         people potentially interested in becoming one. I would also like to use this to meet and learn from fellow IGLs and players. 
         <br></br>
+        <br></br>
         
         I've decided to revise and update my guide to potentially give someone out there assuming the role in a team or someone who's interested in a role switch the 
         opportunity to learn from someones beginner mistakes while opening their eyes to the thought process of a slightly more experienced IGL while understanding 
         the work and dedication is takes to become a successful IGL.  
         <br></br>
+        <br></br>
         Quick background to anyone interested. I just finished my 11th season as an IGL. My team managed to make playoffs in Main but fell in the first round in a close
         and really fun series 2-1 eliminating us from contention to qualify for advanced. I've led this team since season 20 with a lot of different rosters. The plan is 
         to work our ass off during the off season, rebuild the roster and hopefully make a run in the advanced cups ESEA holds in the offseason. 
+        <br></br>
         <br></br>
         Notice before we begin: I'm far from the absolute best. I'm not saying that I am better than someone else and in no way is my advice 100% correct to everyone and every IGL. 
         This is all completely my opinions, my views, my experiences and everything I've learned or been taught overtime. Feel free to disagree, Feel free to tell me
         you disagree. I'll do my best to explain my opinions and thoughts of being an IGL. My goal is to help one person learn, anything else is just extra. 
         <br></br>
+        <br></br>
         Let's roll. 
         <br></br>
+        <br></br>
         *Becoming an IGL*
+        <br></br>
         <br></br>
         -I'll start by saying this. Becoming an In game leader is more than calling strats. Something that isn't said enough is that being the In Game Leader is also
         a leader roll both in and out of game. You control the attitude of the team, you control the work ethic in game and you control the way the game is played on 
@@ -33,24 +39,29 @@ function GuideText () {
         You have to keep an environment that allows for players to learn from their mistakes, learn from each other, and an environment that they feel comfortable both receiving and giving
         criticism to one another. I should note that this should be for all players of any role. Though, this begins with the IGL in my opinion. 
         <br></br>
+        <br></br>
         -Next thing I would like to touch on is Learning and Understanding what your own team and teammates strenghts and weaknesses are. This seems straight forward
         but it's very vital when building a playbook and map pool for your team. If you have a team that likes fast paced action that forces aim fights, build around that. 
         Don't force them to play a slow paced, execute heavy and laid back passive CT. All you're doing is limiting the players of their own personal strenghts. 
         While playing a balanced style CS is generally considered the best. You can 100% build your teams identity to be mainly aggressive or passive depending on your teammates strenghts. 
         I will 100% advise that if you build your team around agressive or passive play, always be willing to switch up tempo at right times to punish and catch off the opposing team. 
         <br></br>
+        <br></br>
         -I'll begin to go into detail what the role is and what the responsibilities of being IGL entails. Simply put, the IGL primarily lays down the blueprint for round during freezetime. 
         This also can include making mid round decisions based off the flow of the round. Counting Utility, realizing tendencies and adapting throughout the game and even during rounds is 
         some things that can turn you from an average to good or good to great IGL. 
         <br></br>
+        <br></br>
         A lot goes into making this call during freezetime. Let's outline some of the details that should go into your consideration before making the call. While this will 
         seem like a lot. As time goes on, your understanding of the game and the situation will make this a simpler task overtime. Let's list some of the things to think about. 
+        <br></br>
         <br></br>
         First and most importantly Economy. You want ideas for both CT and T for all rounds no matter the economical situation on both sides. Build T strategies and CT setups around your buy 
         and the opposing teams buy. I split my playbook into different T strats based on that. Anti Eco | Eco | Force Buy/Light Buy | Low Utility Gun Round | Full Utility Gun round is the split 
         that I personally use when building a T playbook. On the flipside, I have protocols in place for my CT setups based off the same potential scenarios. If I can identify the round, full buy 
         for us vs the opposing teams low utility gun round. I can quickly call the strat or setup that puts my team into the best position to tackle the round from the beginning. I'll go into futher 
         detail why I use this split and why I advise this split especially for newer IGLs and teams. 
+        <br></br>
         <br></br>
         Second thing you can use to help make freezetime calls as an IGL is learning to understand the flow of the game, picking up on tendencies, finding weaknesses and setting plays up. Let's dive 
         into each one as learning these can help vastly improve your freezetime calls. Below, I'll go into greater detail describing these 4 details 
@@ -61,6 +72,7 @@ function GuideText () {
         can be controlled throughout the round. Maybe even on T side call a passive default while posting your awp on a position where CTs have gotten aggressive in rounds past. These tempo changing 
         rounds can be utilized if you feel the opposing team is getting too complacent in what they're doing and you feel them getting too comfortable. 
         <br></br>
+        <br></br>
         -Second will be picking up on opposing teams tendencies. For this, we'll use Nuke as an example. Commonly used are the outside wall smokes that block off CT vision to the secret stairs. 
         If you can gather the information that the CT who is usual in the mini garage that connects from outside to the A site is the player that drops through the vents to pick up lower. This is 100% 
         a tendency that you can abuse. Simply throwing the wall smokes outside, showing the CTs that you could potentially be outside going lower and waiting until you hear the mini player dropping through 
@@ -69,12 +81,14 @@ function GuideText () {
         angle, calling the same executes at the same time in the round and many many more. Understanding what some tendencies are maps and knowing how you could potentially take advantage of it could net 
         you a player advantage and even sometimes rounds. 
         <br></br>
+        <br></br>
         -The third thing we'll go into is finding weaknesses and having a plan on how to abuse it. It's as easy as it sounds though you need to have a plan in place to abuse weaknesses. 
         Usually weaknesses are the same across each map. Something like CT's not understanding how to counter a middle control default on mirage or cache. Could even be that T sides are giving up portions 
         of control of a side of the map. Banana on Inferno being the prime example. Finding these weaknesses then having a plan in place to abuse them is vital. If you know the opposing 
         CT side is giving up Banana control, you can call a variety of strats to abuse that. Simply throwing fake executes and quickly attacking the A site or throwing utility to show you're executing the B site 
         but instead just attempting to bait out counter utility then reexecuting after the mollies and smokes have disappeared from the CT side. Both are simple strategies you can have in place to abuse a teams 
         weakness. It is simple and very easy to understand. Though having a plan in place can help you be a step even farther ahead. 
+        <br></br>
         <br></br>
         -Lastly is understanding how to set up plays for future rounds. Something I do not see talked about enough is you can 100% condition a team to your own strats and defaults then use that comfortability 
         to your advantage. If you use specific utility on your default on T side for example, you can and should 100% have a strat to abuse them getting content with your default. My own team runs the now 
@@ -85,6 +99,7 @@ function GuideText () {
         lazy on it. It was a strat that started rounds ago, that we wanted to build then use. You can do this on nearly every map on a T side default. Condition a team with a smoke to the point they don't 
         worry when it's thrown then show them that they need to fear a potential group explosion onto a site. This can also open up other avenues of the map. If that second B player is worried about 
         another hit like what happened before, he can be hesitate to contest middle which allows your team the ability to take control of middle a lot easier. 
+        <br></br>
         <br></br>
         Let's talk about the mid round decision. This usually relates to the decision made between the 1:20 mark and 30 second mark on where to attack on T or where to lean the majority of players on CT.  
         You take the information relayed to you by your teammates, the concreate info given up by the opposing team such as known locations of players, nades thrown or previous positions controled by the opposing 
