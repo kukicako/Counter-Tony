9e1bb988bbe74a3b5b9695039f03f0d84c63972f
Gatrrr <davidgatrmacaulay@gmail.com>
2020-04-08 03:44:00 -0700

    Began Round Awareness

--- src/Components/Guides/InGameLeading/roundawarenesstext.js
+++ src/Components/Guides/InGameLeading/roundawarenesstext.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function RoundAwarenessText() {
     return(
-        <p className="GuideText"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
+        <p className="GuideText"> Let's start to dive into some more specific topics within IGLing. First I'll start off by talking about Round awareness. I define this   </p>
     )
 }
 
