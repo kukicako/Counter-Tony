e2fcd674ff2b7084314aa2c5fd85b57bebaa49bf
Muamer Kukic <kukicako@gmail.com>
2020-04-17 02:43:08 -0700

    styling

--- src/Components/Homepage/nav.js
+++ src/Components/Homepage/nav.js
@@ -1,73 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
+import './nav.scss'
 
-const NavBar = styled.nav`
-    display:flex;
-    width:100%;
-    height:4em;
-    background: white;
-    color: blue;
-    `
-;
-
-
-const NavTitle = styled.h3`
-    font-size: xx-large;
-    padding-top:5%;
-    text-decoration: none;
-    &:hover {
-        color:blue !important
-    }
-    
-`
-;
-const NavLeft = styled.div`
-    display:flex;
-    flex-wrap:wrap;
-    width:50%
-    `
-;
-
-
-
-const NavRight = styled.div`
-    display:flex;
-    flex-wrap:wrap;
-    width:60%;
-    justify-content: flex-end;
-    align-items: center;
-
-    `
-;
-
-
-
-const StyledP = styled.a`
-    width:18%;
-    padding:2%;
-    color: blue;
-    font-size: large;
-
-
-    `
-;
 
 const Nav = () => {
     return (
-        <NavBar>
-           <NavLeft>
+        <nav className="NavBar">
+           <div className="NavLeft">
                 
-                <Link to="/"><NavTitle>CSGO-Teacher</NavTitle></Link>    
-           </NavLeft>
+                <Link to="/"><h3 className="NavTitle">CSGO-Teacher</h3></Link>    
+           </div>
 
-           <NavRight>
-               <StyledP><Link to="/Guides">Guides</Link> </StyledP>
-               <StyledP href="https://www.w3schools.com">Streams</StyledP>
-               <StyledP href="https://www.w3schools.com">Teachers</StyledP>
+           <div className="NavRight">
+               <a className="StyledP" ><Link className="StyledP" to="/Guides">Guides</Link> </a>
+               <a className="StyledP" href="https://www.w3schools.com">Streams</a>
+               <a className="StyledP" href="https://www.w3schools.com">Teachers</a>
 
-           </NavRight>
-       </NavBar>
+           </div>
+       </nav>
     );
 }
 
