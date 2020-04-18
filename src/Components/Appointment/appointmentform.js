496aec053315edaea754c1a6fe5d7e697e4be24e
Muamer Kukic <kukicako@gmail.com>
2020-04-17 18:46:49 -0700

    question not required

--- src/Components/Appointment/appointmentform.js
+++ src/Components/Appointment/appointmentform.js
@@ -12,13 +12,13 @@ const appointmentform = props => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
             <form className='form'>
-                    <label className='label'>Name: *
+                    <label className='label'>Name:
                         <input className='input' type='text' name='name' placeholder='John Doe' required />
                     </label>
                     <div className="label-text">
                         <p>Real Name</p>
                     </div> 
-                    <label className='label'>Email: *
+                    <label className='label'>Email:
                         <input className='input' type='text' name='email' placeholder='john@mail.com' required />
                     </label>
                     <div className="label-text">
@@ -30,14 +30,14 @@ const appointmentform = props => {
                     <div className="label-text">
                         <p>Please list all your ranks silver/rank S/Faceit lvl 10</p>
                     </div> 
-                    <label className='label'>Steam ID: *
+                    <label className='label'>Steam ID:
                         <input className='input-last' type='text' name='steam' placeholder='https:/steamcommunity.com/profiles/76561198149671515' required />
                     </label>
                     <div className="label-text">
                         <p>Click <a href="https://steamidfinder.com">here</a> to find your steam id</p>
                     </div>     
                     <label className='label'>Questions:
-                        <input className='input-last' type='text' name='question' placeholder='...' required />
+                        <input className='input-last' type='text' name='question' placeholder='...'  />
                     </label> 
                     
                     <div className="submit-appointment">
