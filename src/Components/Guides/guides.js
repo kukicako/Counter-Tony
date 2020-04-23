f40c776215678aa46c386c0ccac3e79b8c554dca
Gatrrr <davidgatrmacaulay@gmail.com>
2020-04-23 05:52:58 -0700

    new guide page design created

--- src/Components/Guides/guides.js
+++ src/Components/Guides/guides.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import GuideTitle from './guidetitle.js'
 import GuideText from './guidetext.js'
+import GuideNav from './guidesnavbar.js'
 
 function Guides() {
     return(
-        <div className='GuidePage'>
-            <div className='GuidePageStyling'>
-                <GuideTitle />
-                <GuideText />
+        <div>
+            <div>
+                <GuideNav />
+            </div>
+            <div className='GuidePage'>
+                <div className='GuidePageStyling'>
+                    <GuideTitle />
+                    <GuideText />
+                </div>
             </div>
         </div>
     );
---
+++ src/Components/Guides/guidesnavbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './guidenav.scss'
+
+
+const GuideNav = () => {
+    return (
+        <nav className="NavBar">
+           <div className="Nav">
+               <p>Work in Progress</p>
+           </div>
+        </nav>
+    );
+}
+
+
+export default GuideNav;
