d97bd7600aa85de1c100a351daa64f3e378e760d
Gatrrr <davidgatrmacaulay@gmail.com>
2020-04-26 05:31:33 -0700

    Introduction almost complete

--- src/Components/Guides/Introduction/Learn.js
+++ src/Components/Guides/Introduction/Learn.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function LearnText () {
     return(
-        <p className="GuideText"> Yes. Yes. </p>
+        <div>
+            <div className="GuideTextPara">
+                <h5>What will you get from CSGOTeacher?</h5>
+                <p>We have over 40 guides in which we cover all positions within the game, the jobs of each position, what each position requires to be successful and other individual mechanics such as Aiming and Utility. Our goal to cover all aspects within a position so that no matter your level, if you're looking to take that next step into competitive counter-strike or if you've been competiting for some. You can and will learn something here. </p>
+            </div>
+        </div>
     )
 }
 
