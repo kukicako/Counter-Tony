11e897a8c3244d8ce2d8443a427df4133be625b0
Muamer Kukic <kukicako@gmail.com>
2020-04-28 13:58:44 -0700

    module status a bit better

--- src/Components/Homepage/guide-card.js
+++ src/Components/Homepage/guide-card.js
@@ -50,7 +50,7 @@ const Guide = props => {
             data-aos-once="true">
             <img className="leading-pic" src={drawing} alt="leading pic" />
             <div className="top-left-text">
-              <h2 className="text-titles-homepage">Help you Improve individually! </h2>
+              <h2 className="text-titles-homepage">Help You Improve Individually! </h2>
               <p>We have taken the tips and tricks from current FPL players and put them into guides you can learn and use to help you improve! There are over 30+ detailed guides, personalized one on one training with high level players, and a wide array of videos stemming from simple aim mechanics to strategies top teams have employed at the highest levels of Counter Strike.</p>
             </div>
           </div>
@@ -65,7 +65,7 @@ const Guide = props => {
           data-aos-once="true">
             <img className="lurk-pic" src={igl1} alt="lurk pic" />
             <div className="middle-right-text">
-              <h2 className="text-titles-homepage">Learn </h2>
+              <h2 className="text-titles-homepage">Become a Leader</h2>
               <p>The orignal inspiration for this site. In-Game Leading is arguably one of the toughest roles in Counter Strike Global Offensive and can be the difference between your team advancing further or falling just short of the finish line. Its a dying role as it is so difficult to learn and manage, so we offer 7 guides written by Gatr who's spent the last 15 League Seasons thriving as an IGL, specifically created to help other IGLs!</p>
             </div>
           </div>
@@ -80,7 +80,7 @@ const Guide = props => {
           data-aos-once="true">
             <img className="entry-pic" src={entry} alt="entry pic" />
             <div className="middle-left-text">
-              <h2 className="text-titles-homepage">Learn </h2>
+              <h2 className="text-titles-homepage">Rotate and Communicate</h2>
               <p >There is a wide array of roles in competitive Counter Strike and each role plays a crucial part to the success of your team and your career. Each role needs to understand it's job and needs to learn how to communicate effectively what they are doing in their role. Read specialized guides specifically created for each role to fully understand what is being asked from you on a round to round basis!</p>
             </div>
           </div>
@@ -95,8 +95,8 @@ const Guide = props => {
           data-aos-once="true">
             <img className="sniper-pic" src={sniper} alt="sniper pic" />
             <div className="bottom-right-text">
-              <h2 className="text-titles-homepage">Learn </h2>
-              <p >Make sure to check out our other dozens of guides for anything and everything competitive counter strike related written by Gatr with help from current FPL and MDL+ players!</p>
+              <h2 className="text-titles-homepage">Think Like A Pro</h2>
+              <p >Make sure to check out our other dozens of guides for anything and everything competitive Counter Strike related written by Gatr with help from current FPL and MDL+ players! We also have our blog where we update current events in the Counter Strike world so that we can keep you up to to date on the latest meta or schedule an appointment with one of our high quality teachers.</p>
             </div>
           </div>
 
