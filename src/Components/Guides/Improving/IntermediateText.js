71b03cfec9c63f30e73da85b70df779a1f73884e
Gatrrr <davidgatrmacaulay@gmail.com>
2020-05-09 16:26:50 -0700

    Added some intermediate text

--- src/Components/Guides/Improving/IntermediateText.js
+++ src/Components/Guides/Improving/IntermediateText.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function IntermediateText () {
     return(
-        <div className="GuideTextPara">
-            <h5>Intermediate Guide:</h5>
-            <p>Test</p>
+        <div>
+            <div className="GuideTextPara">
+                <h5>Intermediate Guide:</h5>
+                <p>This is part 2 of a 4 part series where I will outline some specifics you should focus on learning, improving or getting into depending roughly on your current skill level. Part 1 covered the basics for beginners, this part will be designed for players in the skill range of AK to somewhere from LEM-Global but hasn't started to look into third party services. We will talk about some more specifics that was touched in the first part while beginning to speak about third party services such as Faceit and ESEA. The next part will be for people consistently in that Supreme/Global range who is starting to play on third party services. The final part will be for players who are looking to get into league play, how to get into it, talking about trying out and joining teams. </p>
+                {/* <p>This will be part 1 of a 4 part series where I will outline some specifics you should focus on learning, improving or getting into depending on roughly where your skill level is. Part 1 which is the beginner guide is going to be outlined for someone in the skill range from new to the gold nova ranks. All these ranks are people who are just learning and understanding aspects of the game. The intermediate guides are designed for people in the range of AK to the Global Range that hasn't yet began to start playing third party services such as FaceIt and ESEA. Experienced will be people who are in the Supreme/Global range who is getting into ESEA/FaceIt play. Advanced are players who are looking to get into League play on top of beginning to grind clubs/leaderboards such as ESEA A+ learboards/Rank G and Faceit FPLC/FPLC qualifiers.</p> */}
+            </div>
         </div>
     )
 }
