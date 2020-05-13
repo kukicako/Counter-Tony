a7f4e14d8a6e18235313085c38bc67489bee929b
Gatrrr <davidgatrmacaulay@gmail.com>
2020-05-13 03:22:28 -0700

    Added text, fixing titles/adding images

--- src/Components/Guides/Entry/IntroductionEntryHome.js
+++ src/Components/Guides/Entry/IntroductionEntryHome.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import IntroductionEntryTitle from './IntroductionEntryTitle.js'
 import IntroductionEntryText from './IntroductionEntryText.js'
 import CustomizedGuideSideBar from '../GuideNavbar/guidesnavbar.js'
 
@@ -12,7 +11,6 @@ function IntroductionEntryHome() {
             </div>
             <div className='GuidePage'>
                 <div className='GuidePageStyling'>
-                    <IntroductionEntryTitle />
                     <IntroductionEntryText />
                 </div>
             </div>
