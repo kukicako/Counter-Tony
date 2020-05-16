4c3ecceddaa79355817d7a9d6d020601f4e6d77c
Gatrrr <davidgatrmacaulay@gmail.com>
2020-05-16 03:55:26 -0700

    Added text to 5 sections

--- src/Components/Guides/Entry/IntroductionEntryHome.js
+++ src/Components/Guides/Entry/IntroductionEntryHome.js
@@ -9,6 +9,11 @@ function IntroductionEntryHome() {
             <div>
                 <CustomizedGuideSideBar />
             </div>
+            <div className="GuideTitles">
+                <h4 className="GuideTitlesH4">Guides - Entry</h4>
+                <h1 className="GuideTitlesH1">Introduction to Entry Fragging</h1>
+                <h3 className="GuideTitlesH3">Written by: David "Gatr" Downes </h3>
+            </div>
             <div className='GuidePage'>
                 <div className='GuidePageStyling'>
                     <IntroductionEntryText />
